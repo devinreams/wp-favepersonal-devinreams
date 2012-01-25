e7236f34e32ec3075af0f3e808060eb52be338f4
Devin Reams <devin@reams.me>
2012-01-24 23:16:26 -0800

    move color styles into child theme, why did I import the parent styles, and disable the color style from outputting in wp_head

--- functions.php
+++ functions.php
@@ -17,12 +17,20 @@ function akv3_social_comment_broadcast_format($format, $comment, $service) {
 }
 add_filter('social_comment_broadcast_format', 'akv3_social_comment_broadcast_format', 10, 3);
 
+// move comment form after the comments themselves
 function akv3_social_comment_block_order($order) {
 	return array('comments', 'form');
 }
 add_filter('social_comment_block_order', 'akv3_social_comment_block_order');
 
+// don't use ODP content when listing my site
 function fpdr_noodp() {
 	echo "<meta name=\"robots\" content=\"noodp\" />\n";
 }
 add_action('wp_head','fpdr_noodp');
+
+// don't output custom colors, they're in styles.css
+function fpdr_nocolors() {
+	remove_action('wp_head', 'cfcp_color_css', 8);
+}
+add_action('wp_enqueue_scripts', 'fpdr_nocolors');
